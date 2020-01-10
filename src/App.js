eea6af34b961f5e31882bc56b35b85bb0c78da18
donna <donnacamos88@gmail.com>
2020-01-10 15:32:29 -0500

    imported packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux' 
 
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/titles', {
+      method: 'GET'
+    })
+    .then(res => res.json())
+    .then(data => console.log(data)) 
+  }
+
   render() {
     return (
       <div className="App">
